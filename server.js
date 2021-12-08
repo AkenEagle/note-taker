ed4bb5121fed01ece2f55a909ee0206abe2ea244
AkenEagle <akeneagle@gmail.com>
2021-12-08 20:49:07 +0000

    New note and delete note working

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const { v4: uuidv4 } = require("uuid");
 const PORT = process.env.port || 3000;
 const app = express();
 
-// Route for index file
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
+// Middlewares
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Route for notes application
 app.get("/notes", (req, res) => {
@@ -18,21 +18,65 @@ app.get("/notes", (req, res) => {
 
 // Get saved notes
 app.get("/api/notes", (req, res) => {
-  const data = require("./db/db.json");
-  res.json(data);
+  // Read the db.json
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    // Send the data as JSON object
+    res.send(JSON.parse(data));
+  });
 });
 
 // New note
 app.post("/api/notes", (req, res) => {
-  const noteId = uuidv4();
-  console.log(req.body);
-  const noteTitle = req.body.title;
-  const noteText = req.body.text;
+  // Get the user note
+  const userNote = { id: uuidv4(), title: req.body.title, text: req.body.text };
 
-  const userNote = { id: noteId, title: noteTitle, text: noteText };
+  // Read the db.json
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    // Parse to JSON
+    let notes = JSON.parse(data);
+    // Push the user note
+    notes.push(userNote);
+    // Write to db.json the new notes
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+      if (err) {
+        console.log(error);
+        return;
+      }
+    });
+    // Send the new note to client
+    res.send(userNote);
+  });
+});
 
-  // Add note to db.json
-  fs.appendFileSync("./db/db.json", userNote);
+// Delete a note by ID
+app.delete("/api/notes/:id", (req, res) => {
+  // Get the ID of the note to delete
+  const noteID = req.params.id;
+
+  // Read from db.json
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    // Get the notes from the file in JSON
+    let notes = JSON.parse(data);
+
+    // Delete the note with that ID in a new array
+    let newNotes = notes.filter((note) => note.id !== noteID);
+
+    // Write the new notes in db.json
+    fs.writeFile("./db/db.json", JSON.stringify(newNotes), (err) => {
+      if (err) {
+        console.log(error);
+        return;
+      }
+    });
+
+    // Send
+    res.send(newNotes);
+  });
+});
+
+// Route for index file
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.listen(PORT, () => {
