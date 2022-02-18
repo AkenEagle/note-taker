b2a9845f484860aa352d8b8bbfca882c1ecca70e
AkenEagle <akeneagle@gmail.com>
2022-02-18 18:21:58 +0000

    Updated readme

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Middlewares
